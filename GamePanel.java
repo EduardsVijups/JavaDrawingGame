aae5af30633d455ecc310296f0331d6debfdbf0a
EduardsVijups <48444373+EduardsVijups@users.noreply.github.com>
2024-12-26 17:44:07 +0200

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,277 +1,186 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.Timer;
+import java.io.*;
 
-public class GamePanel extends JPanel implements ActionListener {
-   private static final long serialVersionUID = 3876286449627408974L;
-   static final int WIDTH = 600;
-   static final int HEIGHT = 600;
-   static final int UNIT_SIZE = 25;
-   static final int GAME_UNITS = 14400;
-   static final int DELAY = 75;
-   int x = 0;
-   int y = 0;
-   long[][] blockPos;
-   Random random = new Random();
-   Timer timer;
-   int blocks = 0;
-   int rows = 24;
-   int colls = 24;
-   int selected = 1;
-   String command;
-   String clear = "clear";
-   final JFrame parent = new JFrame();
-   JButton button = new JButton();
+public class GamePanel extends JPanel implements ActionListener{
+    @Serial
+    private static final long serialVersionUID = 3876286449627408974L;
+    static final int WIDTH = 600;
+    static final int HEIGHT = 600;
+    static final int UNIT_SIZE = 50;
+    static final int GAME_UNITS = (WIDTH*HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    int x = 0;
+    int y = 0;
+    long[][] blockPos;
+    Random random;
+    Timer timer;
 
-   GamePanel() {
-      this.setPreferredSize(new Dimension(600, 600));
-      this.setBackground(Color.black);
-      this.setFocusable(true);
-      this.addKeyListener(new GamePanel.MyKeyAdapter());
-      this.startGame();
-   }
+    int rows = HEIGHT/UNIT_SIZE;
+    int colls = WIDTH/UNIT_SIZE;
+    int selected = 1;
+    String command;
 
-   public static void main(String[] args) {
-   }
+    final JFrame parent = new JFrame();
+    //JButton button = new JButton();
 
-   public void startGame() {
-      this.timer = new Timer(75, this);
-      this.timer.start();
-      this.blockPos = new long[this.rows][this.colls];
-   }
 
-   public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      this.draw(g);
-   }
 
-   public void fillIn(Graphics g, Color c) {
-      g.setColor(Color.c);
-      g.fillRect(i * 25, j * 25, 25, 25);
-   }
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+    public static void main(String[] args) {
 
-   public void draw(Graphics g) {
-      for(int l = 0; l < this.rows; ++l) {
-         g.drawLine(l * 25, 0, l * 25, 600);
-         g.drawLine(0, l * 25, 600, l * 25);
-      }
-      Color[] blockColors = {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta, Color.white, Color.gray, Color.black};
-      String[] blockID = {"1L", "2L", "3L", "4L", "5L", "6L", "7L", "8L", "9L"};
-
-      for(int i = 0; i < this.rows; ++i) {
-         for(int j = 0; j < this.colls; ++j) { 
-            for (int k = 0; k < blockID.length; k++) {
-               if (blockID[i] == this.blockPos[i][j]) {
-                  g.setColor(blockColors[i]);
-                  g.fillRect(i * 25, j * 25, 25, 25);
-               }
+    }
+    public void startGame() {
+        timer = new Timer(DELAY,this);
+        timer.start();
+        blockPos = new long[rows][colls];
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g) {
+        Color[] blockColors = {Color.red, Color.orange, Color.yellow, Color.green, Color.blue, Color.magenta, Color.white, Color.gray, Color.black};
+        for(int i = 0; i<rows;i++) {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, WIDTH, i*UNIT_SIZE);
+        }
+        for(int i = 0; i<rows; i++) {
+            for(int j = 0; j<colls; j++) {
+                for(int clrCheck = 0; clrCheck<blockColors.length; clrCheck++){
+                    if(blockPos[i][j] == clrCheck+1){
+                        g.setColor(blockColors[clrCheck]);
+                        g.fillRect(i*UNIT_SIZE, j*UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+                    }
+                }
             }
-         }
-      }
-
-      this.parseCommand();
-      g.setColor(Color.DARK_GRAY);
-      g.fillRect(this.x * 25, this.y * 25, 25, 25);
-   }
-
-   public void paintBlock(int block, int row, int coll) {
-      if (this.blockPos[row][coll] != (long)block) {
-         this.blockPos[row][coll] = (long)block;
-      } else if (this.blockPos[row][coll] == (long)block) {
-         this.blockPos[row][coll] = 0L;
-      }
-
-   }
-
-   public void parseCommand() {
-      if (this.command != null) {
-         int i;
-         if (this.command.equals("clear") || this.command.equals("clr")) {
-            int i = 0;
-
-            while(true) {
-               if (i >= this.rows) {
-                  this.command = null;
-                  break;
-               }
-
-               for(i = 0; i < this.colls; ++i) {
-                  this.blockPos[i][i] = 0L;
-               }
-
-               ++i;
+        }
+        parseCommand();
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(x*UNIT_SIZE, y*UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+    }
+    public void paintBlock(int block, int row, int coll) {
+        if (blockPos[row][coll] != block) {
+            blockPos[row][coll] = block;
+        } else if (blockPos[row][coll] == block){
+            blockPos[row][coll] = 0;
+        }
+    }
+    public void parseCommand() {
+        if(command != null) {
+            if(Objects.equals(command, "clear") || Objects.equals(command, "clr")) {
+                for(int i = 0; i < rows;i++) {
+                    for(int j = 0; j < colls; j++) {
+                        blockPos[j][i] = 0;
+                    }
+                }
+                command = null;
             }
-         }
+            if(Objects.equals(command, "save")) {
+                try {
+                    FileWriter saveFile = new FileWriter("save.txt");
+                    for(int i = 0; i < rows;i++) {
+                        for(int j = 0; j < colls; j++) {
+                            saveFile.write(Long.toString(blockPos[j][i]));
+                        }
+                        saveFile.write("\n");
+                    }
+                    saveFile.close();
+                    System.out.println("Successfully saved map to the file.");
 
-         int i;
-         if (this.command.equals("save")) {
-            try {
-               FileWriter saveFile = new FileWriter("save.txt");
-
-               for(i = 0; i < this.rows; ++i) {
-                  for(i = 0; i < this.colls; ++i) {
-                     saveFile.write(Long.toString(this.blockPos[i][i]));
-                  }
-
-                  saveFile.write("\n");
-               }
-
-               saveFile.close();
-               System.out.println("Successfully saved map to the file.");
-            } catch (IOException var7) {
-               System.out.println("An error occurred.");
-               var7.printStackTrace();
+                } catch (IOException e){
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+                command = null;
             }
-
-            this.command = null;
-         }
-
-         if (this.command.equals("load")) {
-            try {
-               File loadFile = new File("load.txt");
-               Scanner myReader = new Scanner(loadFile);
-
-               while(myReader.hasNextLine()) {
-                  for(i = 0; i < this.rows; ++i) {
-                     String data = myReader.nextLine();
-
-                     for(int j = 0; j < this.colls; ++j) {
-                        this.blockPos[j][i] = Long.parseLong(data.substring(j, j + 1));
-                     }
-                  }
-               }
-            } catch (FileNotFoundException var6) {
-               var6.printStackTrace();
+            if(Objects.equals(command, "load")) {
+                try {
+                    File loadFile = new File("load.txt");
+                    Scanner myReader = new Scanner(loadFile);
+                    while (myReader.hasNextLine()) {
+                        for(int i = 0; i < rows;i++) {
+                            String data = myReader.nextLine();
+                            for(int j = 0; j < colls; j++) {
+                                blockPos[j][i] = Long.parseLong(data.substring(j, j+1));
+                            }
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                command = null;
+                System.out.println("Printed file data");
             }
-
-            this.command = null;
-            System.out.println("Printed file data");
-         }
-
-         this.command = null;
-      }
-
-   }
-
-   public void actionPerformed(ActionEvent e) {
-      this.repaint();
-   }
-
-   public class MyKeyAdapter extends KeyAdapter {
-      public void keyPressed(KeyEvent e) {
-         int i;
-         int j;
-         switch(e.getKeyCode()) {
-         case 10:
-            for(i = 0; i < GamePanel.this.rows; ++i) {
-               for(j = 0; j < GamePanel.this.colls; ++j) {
-                  System.out.print(GamePanel.this.blockPos[j][i]);
-               }
-
-               System.out.println();
+            command = null;
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_A, KeyEvent.VK_LEFT -> {
+                    if (x > 0) {x -= 1;}
+                }
+                case KeyEvent.VK_D, KeyEvent.VK_RIGHT -> {
+                    if (x < colls - 1) {x += 1;}
+                }
+                case KeyEvent.VK_W, KeyEvent.VK_UP -> {
+                    if (y > 0) {y -= 1;}
+                }
+                case KeyEvent.VK_S, KeyEvent.VK_DOWN -> {
+                    if (y < rows - 1) {y += 1;}
+                }
+                case KeyEvent.VK_SPACE -> {
+                    for (int i = 0; i < rows; i++) {
+                        if (i == x) {
+                            for (int j = 0; j < colls; j++) {
+                                if (j == y) {
+                                    paintBlock(selected, i, j);
+                                }
+                            }
+                        }
+                    }
+                }
+                case KeyEvent.VK_1 -> {
+                    selected = 1;
+                    System.out.println(command);
+                }
+                case KeyEvent.VK_2 -> selected = 2;
+                case KeyEvent.VK_3 -> selected = 3;
+                case KeyEvent.VK_4 -> selected = 4;
+                case KeyEvent.VK_5 -> selected = 5;
+                case KeyEvent.VK_6 -> selected = 6;
+                case KeyEvent.VK_7 -> selected = 7;
+                case KeyEvent.VK_8 -> selected = 8;
+                case KeyEvent.VK_9 -> selected = 9;
+                case KeyEvent.VK_ENTER -> {
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < colls; j++) {
+                            System.out.print(blockPos[j][i]);
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("\n");
+                }
+                case KeyEvent.VK_CONTROL -> command = JOptionPane.showInputDialog(parent, "Enter Command", null);
             }
-
-            System.out.println("\n");
-            break;
-         case 17:
-            GamePanel.this.command = JOptionPane.showInputDialog(GamePanel.this.parent, "Enter Command", (Object)null);
-            break;
-         case 32:
-            for(i = 0; i < GamePanel.this.rows; ++i) {
-               if (i == GamePanel.this.x) {
-                  for(j = 0; j < GamePanel.this.colls; ++j) {
-                     if (j == GamePanel.this.y) {
-                        GamePanel.this.paintBlock(GamePanel.this.selected, i, j);
-                     }
-                  }
-               }
-            }
-
-            return;
-         case 37:
-            if (GamePanel.this.x > 0) {
-               --GamePanel.this.x;
-            }
-            break;
-         case 38:
-            if (GamePanel.this.y > 0) {
-               --GamePanel.this.y;
-            }
-            break;
-         case 39:
-            if (GamePanel.this.x < GamePanel.this.colls - 1) {
-               ++GamePanel.this.x;
-            }
-            break;
-         case 40:
-            if (GamePanel.this.y < GamePanel.this.rows - 1) {
-               ++GamePanel.this.y;
-            }
-            break;
-         case 49:
-            GamePanel.this.selected = 1;
-            System.out.println(GamePanel.this.command);
-            break;
-         case 50:
-            GamePanel.this.selected = 2;
-            break;
-         case 51:
-            GamePanel.this.selected = 3;
-            break;
-         case 52:
-            GamePanel.this.selected = 4;
-            break;
-         case 53:
-            GamePanel.this.selected = 5;
-            break;
-         case 54:
-            GamePanel.this.selected = 6;
-            break;
-         case 55:
-            GamePanel.this.selected = 7;
-            break;
-         case 56:
-            GamePanel.this.selected = 8;
-            break;
-         case 57:
-            GamePanel.this.selected = 9;
-            break;
-         case 65:
-            if (GamePanel.this.x > 0) {
-               --GamePanel.this.x;
-            }
-            break;
-         case 68:
-            if (GamePanel.this.x < GamePanel.this.colls - 1) {
-               ++GamePanel.this.x;
-            }
-            break;
-         case 83:
-            if (GamePanel.this.y < GamePanel.this.rows - 1) {
-               ++GamePanel.this.y;
-            }
-            break;
-         case 87:
-            if (GamePanel.this.y > 0) {
-               --GamePanel.this.y;
-            }
-         }
-
-      }
-   }
+        }
+    }
 }
